f6274cacbf5ea559ac6c50ada7a2024f7b262afb
[Sean Hogan] <[shogan@uwsp.edu]>
2026-04-29 15:31:12 -0500

    ﻿{"uwsp":1.3,"desc":"Replaced all ddMMyyyy with yyyyMMdd","tags":["draft","analyze","split"]}

--- Driver Automation Tool/UI/MainApplication.ps1
+++ Driver Automation Tool/UI/MainApplication.ps1
@@ -5518,7 +5518,7 @@ $btn_RefreshModels.Add_Click({
                                     Baseboards = $Model.SystemId
                                     OS         = $WindowsVersion
                                     'OS Build' = $WindowsBuild
-                                    Version    = (Get-Date -Format 'ddMMyyyy')
+                                    Version    = (Get-Date -Format 'yyyyMMdd')
                                 }
                             }
                         }
@@ -5760,7 +5760,7 @@ $btn_RefreshModels.Add_Click({
                                     Baseboards = $Model
                                     OS         = $WindowsVersion
                                     'OS Build' = $WindowsBuild
-                                    Version    = (Get-Date -Format 'ddMMyyyy')
+                                    Version    = (Get-Date -Format 'yyyyMMdd')
                                 }
                             }
                         }
@@ -11563,7 +11563,7 @@ $btn_CustomBuild.Add_Click({
         }
     })
 
-    $customVersion = Get-Date -Format "ddMMyyyy"
+    $customVersion = Get-Date -Format "yyyyMMdd"
     [void]$script:CustomBuildPS.AddArgument((Resolve-Path $modulePath).Path)
     [void]$script:CustomBuildPS.AddArgument($make)
     [void]$script:CustomBuildPS.AddArgument($model)
